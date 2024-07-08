eb1acfef430c6b8beae14c060cc2e848a938eb65
SuparnaAishee <suparnad806Qgmail.com>
2024-07-08 20:07:51 +0600

    first commit

--- dist/app/middlewares/handle.error.js
+++ dist/app/middlewares/handle.error.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.notFoundHandler = void 0;
+//error handdler
 const notFoundHandler = (req, res, next) => {
     const error = new Error();
     error.name = 'not-found';
